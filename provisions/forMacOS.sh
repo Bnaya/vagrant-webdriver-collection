095ef8e716a4c18b02359c4226c755e76b93a680
Bnaya Peretz <me@bnaya.net>
2020-04-12 11:39:17 +0300

    This is actually much better option than the automator app

---
+++ provisions/forMacOS.sh
@@ -0,0 +1,4 @@
+set -x
+echo "Should be run by The terminal app";
+webdriver-manager start
+echo "Done Should be run by The terminal app";
--- provisions/macosAlways.sh
+++ provisions/macosAlways.sh
@@ -1,6 +1,5 @@
 set -x
 echo "Always script";
 echo "webdriver-manager auto startup will work only on the second start";
-cd /vagrant;
-open webdriver-manager_start.app;
+open -a "Terminal" "/vagrant/provisions/forMacOS.sh"
 echo "Done Always script";
